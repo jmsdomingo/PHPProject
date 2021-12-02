540972d08860cda7da05f7fedff7af9184dc71cc
Jm <jannmichaelsdomingo@gmail.com>
2021-12-02 23:26:52 +0800

    init

---
+++ about.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css"> 
+    <title>COUNT EDU | About</title>  
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <h4>COUNT EDU</h4>
+        </div>
+        <ul class="nav-links">
+            <li>
+                <a href="home.php">Home</a>
+            </li>
+            <li>
+                <a href="exercise.php">Exercises</a>
+            </li>
+            <li>
+                <a href="about.php">About</a>
+            </li>
+            <li>
+                <a href="controller?logout=1">Logout</a>
+            </li>     
+        </ul>
+    </nav>
+    <br><br> <br><br>
+
+    <b><h1 style="text-align:center">Our Team</h1></b>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+            <img src="images/gailtecson.png" alt="Jane" style="width:100%">
+                <div class="container">
+                    <h2>Kimberly Tecson</h2>
+                    <p class="title">Project Manager</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>kimberlygail.tecson.cics@ust.edu.ph</p>
+                </div>
+            </div>
+
+            <div class="card">
+            <img src="images/ejbejer.jpg" alt="Mike" style="width:100%">
+                <div class="container">
+                    <h2>Emmanuel Bejer</h2>
+                    <p class="title">Front-end Developer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>emmanueljames.bejer.cics@ust.edu.ph</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="column">
+            <div class="card">
+            <img src="images/jmdomingo.jpg" alt="John" style="width:100%">
+                <div class="container">
+                    <h2>Jannmichael Domingo</h2>
+                    <p class="title">Back-end Developer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>jannmichael.domingo.iics@ust.edu.ph</p>
+                </div>
+            </div>
+
+            <div class="card">
+            <img src="images/christianabrera.jpg" alt="John" style="width:100%">
+                <div class="container">
+                    <h2>Christian Abrera</h2>
+                    <p class="title">Front-end Developer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>christiancarl.abrera.iics@ust.edu.ph</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>    
