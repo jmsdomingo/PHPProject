8f63b332db5192e0870df64763933f0116250c4d
Abreracc27 <christiancarl.abrera.iics@ust.edu.ph>
2021-12-07 00:05:17 +0800

    up

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
     <div class="container-fluid" id="header">
         <div class="row text-center m-2">
             <div class="col-12">
-                <span id="header-txt">About Us</span>
+                <span id="header-txt"><span class="zero">A</span><span class="one">b</span><span class="two">o</span><span class="three">u</span><span class="four">t</span> <span class="ten">U</span><span class="zero">s</span></span>
             </div>
         </div>
     </div>
@@ -24,9 +24,9 @@
                 <div class="img-container">
                     <img src="images/christianabrera.jpg" alt="Christian Abrera">
                     <br>
-                    <span id="team-desc">Christian Abrera</span>
+                    <span id="team-desc"><span class="zero">Christian Abrera</span></span>
                     <br>
-                    <span id="team-desc2">4ITF</span>
+                    <span id="team-desc2"><span class="zero">4ITF</span></span>
                 </div>
             </div>
             
@@ -34,9 +34,9 @@
                 <div class="img-container">
                     <img src="images/ejbejer.jpg" alt="Emmanuel Bejer">
                     <br>
-                    <span id="team-desc">Emmanuel Bejer</span>
+                    <span id="team-desc"><span class="one">Emmanuel Bejer</span></span>
                     <br>
-                    <span id="team-desc2">4ITF</span>
+                    <span id="team-desc2"><span class="one">4ITF</span></span>
                 </div>
             </div>
 
@@ -44,9 +44,9 @@
                 <div class="img-container">
                     <img src="images/jmdomingo.jpg" alt="Jannmichael Domingo">
                     <br>
-                    <span id="team-desc">Jannmichael Domingo</span>
+                    <span id="team-desc"><span class="four">Jannmichael Domingo</span></span>
                     <br>
-                    <span id="team-desc2">4ITF</span>
+                    <span id="team-desc2"><span class="four">4ITF</span></span>
                 </div>
             </div>
 
@@ -54,9 +54,9 @@
                 <div class="img-container">
                     <img src="images/gailtecson.png" alt="Gail Tecson">
                     <br>
-                    <span id="team-desc">Gail Tecson</span>
+                    <span id="team-desc"><span class="ten">Gail Tecson</span></span>
                     <br>
-                    <span id="team-desc2">4ITF</span>
+                    <span id="team-desc2"><span class="ten">4ITF</span></span>
                 </div>
             </div>
         </div>
